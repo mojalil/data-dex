c218059294fd3535a5deff71119aabd92eeafdc6
motypes <mo@openfren.com>
2023-07-24 12:02:44 +0800

    added unlist, relist and purchase data

--- src/DataContract.sol
+++ src/DataContract.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
+// Errors
+error DataContract__DataNotOwnedBySeller();
+error DataContract__DataAlreadyListed();
+error DataContract__DataNotListed();
+error DataContract__PriceNotMet();
+error DataContract__DataIsAlreadySold();
+
 /**
  * @title A Blockchain-based Data Marketplace
  * @author motypes
  * @notice This contract is a work in progress and is not yet ready for production use
- * @dev Functions listData (allows sellers to list their data), 
- * buyData (allows buyers to purchase listed data), 
- * getOwner (returns the owner of a particular data set), 
+ * @dev Functions listData (allows sellers to list their data),
+ * buyData (allows buyers to purchase listed data),
+ * getOwner (returns the owner of a particular data set),
  * transferOwnership (transfers ownership from seller to buyer).
  */
 
 contract DataContract {
-
     // Type Declarations
     struct Data {
         uint256 id; // Unique ID of the data set
@@ -29,7 +35,30 @@ contract DataContract {
     mapping(uint256 => Data) private s_dataStore; // Maps data set ID to data set
 
     // Events
-    event DataListed(uint256 indexed _id, address indexed _owner, string _metadata, uint256 _price); // Emitted when a data set is listed
+
+    // Emitted when a data set is listed
+    event DataListed(
+        uint256 indexed _id,
+        address indexed _owner,
+        string _metadata,
+        uint256 _price
+    ); 
+
+    // Emitted when a data set is unlisted
+    event DataUnlisted(
+        uint256 indexed _id,
+        address indexed _owner,
+        string _metadata,
+        uint256 _price
+    );
+
+    // Emitted when a data set is purchased
+    event DataPurchased(
+        uint256 indexed _id,
+        address indexed _owner,
+        string _metadata,
+        uint256 _price
+    );
 
     // Functions
     /**
@@ -42,7 +71,7 @@ contract DataContract {
      * 8. getOwner
      * 9. getDataSet
      */
-    
+
     constructor() {
         i_dataIdCounter = 0;
     }
@@ -53,13 +82,107 @@ contract DataContract {
      * @param _metadata Json string containing data set metadata (source, type, date, etc.)
      * @param _price Price for which the data is listed
      */
-    function listData(string memory _metadata, uint256 _price) external returns (uint256) {
+    function listData(
+        string memory _metadata,
+        uint256 _price
+    ) external returns (uint256) {
         i_dataIdCounter++;
-        s_dataStore[i_dataIdCounter] = Data(i_dataIdCounter, payable(msg.sender), _metadata, _price, false, false, true);
+        s_dataStore[i_dataIdCounter] = Data(
+            i_dataIdCounter,
+            payable(msg.sender),
+            _metadata,
+            _price,
+            false,
+            false,
+            true
+        );
         emit DataListed(i_dataIdCounter, msg.sender, _metadata, _price);
         return i_dataIdCounter;
     }
 
+    /**
+     * @notice Allows sellers to unlist their data
+     * @dev Removes a data set from the data store
+     * @param _id ID of the data set
+     */
+
+    function unListData(uint256 _id) external {
+        if (msg.sender != s_dataStore[_id].owner) {
+            revert DataContract__DataNotOwnedBySeller();
+        }
+
+        if (s_dataStore[_id].isListed == false) {
+            revert DataContract__DataNotListed();
+        }
+        s_dataStore[_id].isListed = false;
+
+        emit DataUnlisted(
+            _id,
+            s_dataStore[_id].owner,
+            s_dataStore[_id].metadata,
+            s_dataStore[_id].price
+        );
+
+    }
+
+    /**
+     * @notice Allows sellers to relist their data
+     * @dev Adds a data set back to the data store
+     * @param _id ID of the data set
+     */
+
+    function reListData(uint256 _id) external {
+        if (msg.sender != s_dataStore[_id].owner) {
+            revert DataContract__DataNotOwnedBySeller();
+        }
+
+        if (s_dataStore[_id].isListed == true) {
+            revert DataContract__DataAlreadyListed();
+        }
+        s_dataStore[_id].isListed = true;
+
+        emit DataListed(
+            _id,
+            s_dataStore[_id].owner,
+            s_dataStore[_id].metadata,
+            s_dataStore[_id].price
+        );
+    }
+
+    /**
+     * @notice Allows buyers to purchase listed data
+     * @dev Transfers ownership of a data set from seller to buyer
+     * @param _id ID of the data set
+     */
+
+    function buyData(uint256 _id) external payable {
+
+        // Check to see if the buyer has paid enough
+        if (msg.value != s_dataStore[_id].price) {
+            revert DataContract__PriceNotMet();
+        }
+
+        // Check to see if the data set is listed
+        if (s_dataStore[_id].isListed == false) {
+            revert DataContract__DataNotListed();
+        }
+
+        // Check to see if the data set has been sold
+        if (s_dataStore[_id].isSold == true) {
+            revert DataContract__DataIsAlreadySold();
+        }
+
+        s_dataStore[_id].owner.transfer(msg.value);
+        s_dataStore[_id].isSold = true;
+        s_dataStore[_id].isListed = false;
+
+        emit DataPurchased(
+            _id,
+            s_dataStore[_id].owner,
+            s_dataStore[_id].metadata,
+            s_dataStore[_id].price
+        );
+    }
 
     // Getters
 
@@ -83,12 +206,58 @@ contract DataContract {
     }
 
     /**
+     * @notice Get data set price
+     * @param _id ID of the data set
+     * @return uint256 Price for which the data is listed
+     */
+    function getDataPrice(uint256 _id) external view returns (uint256) {
+        return s_dataStore[_id].price;
+    }
+
+    /**
+     * @notice Get data set verification status
+     * @param _id ID of the data set
+     * @return bool Indicates whether the data set has been verified by the marketplace for accuracy
+     */
+
+    function getDataVerificationStatus(uint256 _id)
+        external
+        view
+        returns (bool)
+    {
+        return s_dataStore[_id].isVerfied;
+    }
+
+    /**
+     * @notice Get data set sold status
+     * @param _id ID of the data set
+     * @return bool Indicates whether the data set has been sold
+     */
+
+    function getDataSoldStatus(uint256 _id) external view returns (bool) {
+        return s_dataStore[_id].isSold;
+    }
+
+    /**
+     * @notice Get data set listed status
+     * @param _id ID of the data set
+     * @return bool Indicates whether the data set is listed for sale
+     */
+
+    function getDataListedStatus(uint256 _id) external view returns (bool) {
+        return s_dataStore[_id].isListed;
+    }
+
+
+    /**
      * @notice Get data set metadata
      * @param _id ID of the data set
      * @return string Json string containing data set metadata (source, type, date, etc.)
      */
 
-    function getDataMetadata(uint256 _id) external view returns (string memory) {
+    function getDataMetadata(
+        uint256 _id
+    ) external view returns (string memory) {
         return s_dataStore[_id].metadata;
     }
 
@@ -104,8 +273,21 @@ contract DataContract {
      * @return bool Indicates whether the data set is listed for sale
      */
 
-    function getDataSet(uint256 _id) external view returns (uint256, address, string memory, uint256, bool, bool, bool) {
-        return (s_dataStore[_id].id, s_dataStore[_id].owner, s_dataStore[_id].metadata, s_dataStore[_id].price, s_dataStore[_id].isVerfied, s_dataStore[_id].isSold, s_dataStore[_id].isListed);
+    function getDataSet(
+        uint256 _id
+    )
+        external
+        view
+        returns (uint256, address, string memory, uint256, bool, bool, bool)
+    {
+        return (
+            s_dataStore[_id].id,
+            s_dataStore[_id].owner,
+            s_dataStore[_id].metadata,
+            s_dataStore[_id].price,
+            s_dataStore[_id].isVerfied,
+            s_dataStore[_id].isSold,
+            s_dataStore[_id].isListed
+        );
     }
-
 }
